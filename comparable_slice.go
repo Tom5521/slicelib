bfbab9320f4eeab34444a8c372058dcfc8922fe3
Tom5521 <thomas.bored5521@gmail.com>
2024-06-28 10:57:32 -0400

    fix: Fix comparable and ordered slice

--- comparable_slice.go
+++ comparable_slice.go
@@ -4,18 +4,75 @@ import (
 	"slices"
 )
 
-type ComparableSlice[T comparable] struct {
-	Slice[T]
+type ComparableSlice[T comparable] []T
+
+func (s ComparableSlice[T]) Slice() []T {
+	return []T(s)
 }
 
+func (s *ComparableSlice[T]) Append(items ...T) {
+	*s = append(*s, items...)
+}
+
+func (s *ComparableSlice[T]) Clear() {
+	*s = ComparableSlice[T]{}
+}
+
+func (s ComparableSlice[T]) Copy() ComparableSlice[T] {
+	return slices.Clone(s)
+}
+
+func (s *ComparableSlice[T]) Insert(index int, items ...T) {
+	*s = slices.Insert(*s, index, items...)
+}
+
+func (s *ComparableSlice[T]) Delete(i, j int) {
+	*s = slices.Delete(*s, i, j)
+}
+
+func (s *ComparableSlice[T]) Pop(index int) {
+	*s = slices.Delete(*s, index, index+1)
+}
+
+func (s *ComparableSlice[T]) Remove(v T) {
+	s.Pop(s.Index(v))
+}
+
+func (s *ComparableSlice[T]) Reverse() {
+	slices.Reverse(*s)
+}
+
+func (s ComparableSlice[T]) IsEmpty() bool {
+	return len(s) == 0
+}
+
+func (s ComparableSlice[T]) Len() int {
+	return len(s)
+}
+
+func (s *ComparableSlice[T]) RemoveDuplicates() {
+	seen := make(map[any]bool)
+	j := 0
+	for i := 0; i < len(*s); i++ {
+		if !seen[(*s)[i]] {
+			seen[(*s)[i]] = true
+			(*s)[j] = (*s)[i]
+			j++
+		}
+	}
+	*s = (*s)[:j]
+}
+
+// CUSTOM METHODS
+
 func (s ComparableSlice[T]) Index(v T) int {
-	return slices.Index(s.Slice, v)
+	return slices.Index(s, v)
 }
 
 func (s ComparableSlice[T]) Contains(v T) bool {
-	return slices.Contains(s.Slice, v)
+	return slices.Contains(s, v)
 }
 
 func (s ComparableSlice[T]) Equal(v ComparableSlice[T]) bool {
-	return slices.Equal(s.Slice, v.Slice)
+	return slices.Equal(s, v)
 }
--- ordered_slice.go
+++ ordered_slice.go
@@ -5,14 +5,83 @@ import (
 	"slices"
 )
 
-type OrderedSlice[T cmp.Ordered] struct {
-	ComparableSlice[T]
+type OrderedSlice[T cmp.Ordered] []T
+
+func (s OrderedSlice[T]) Slice() []T {
+	return []T(s)
+}
+
+func (s *OrderedSlice[T]) Append(items ...T) {
+	*s = append(*s, items...)
+}
+
+func (s *OrderedSlice[T]) Clear() {
+	*s = OrderedSlice[T]{}
+}
+
+func (s OrderedSlice[T]) Copy() OrderedSlice[T] {
+	return slices.Clone(s)
+}
+
+func (s *OrderedSlice[T]) Insert(index int, items ...T) {
+	*s = slices.Insert(*s, index, items...)
+}
+
+func (s *OrderedSlice[T]) Delete(i, j int) {
+	*s = slices.Delete(*s, i, j)
+}
+
+func (s *OrderedSlice[T]) Pop(index int) {
+	*s = slices.Delete(*s, index, index+1)
+}
+
+func (s *OrderedSlice[T]) Remove(v T) {
+	s.Pop(s.Index(v))
+}
+
+func (s *OrderedSlice[T]) Reverse() {
+	slices.Reverse(*s)
+}
+
+func (s OrderedSlice[T]) IsEmpty() bool {
+	return len(s) == 0
+}
+
+func (s OrderedSlice[T]) Len() int {
+	return len(s)
+}
+
+func (s *OrderedSlice[T]) RemoveDuplicates() {
+	seen := make(map[any]bool)
+	j := 0
+	for i := 0; i < len(*s); i++ {
+		if !seen[(*s)[i]] {
+			seen[(*s)[i]] = true
+			(*s)[j] = (*s)[i]
+			j++
+		}
+	}
+	*s = (*s)[:j]
+}
+
+// CUSTOM METHODS
+
+func (s OrderedSlice[T]) Index(v T) int {
+	return slices.Index(s, v)
+}
+
+func (s OrderedSlice[T]) Contains(v T) bool {
+	return slices.Contains(s, v)
+}
+
+func (s OrderedSlice[T]) Equal(v OrderedSlice[T]) bool {
+	return slices.Equal(s, v)
 }
 
 func (s *OrderedSlice[T]) Sort() {
-	slices.Sort(s.Slice)
+	slices.Sort(*s)
 }
 
 func (s OrderedSlice[T]) BinarySearch(v T) (int, bool) {
-	return slices.BinarySearch(s.Slice, v)
+	return slices.BinarySearch(s, v)
 }
